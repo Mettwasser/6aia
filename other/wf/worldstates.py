223f51a6b4ab36da91352b02fe8a83b6ba804785
Mettwasser <kaicrafter2000@gmail.com>
2022-06-17 17:28:01 +0200

    Actually working now

--- other/wf/worldstates.py
+++ other/wf/worldstates.py
@@ -2,6 +2,11 @@ import asyncio, nextcord, aiohttp
 from main import bot_basic_color
 
 
+async def get_req(url: str, session: aiohttp.ClientSession):
+    async with session.get(url) as response:
+        return await response.json()
+
+
 async def cycles():
     links = [
         "https://api.warframestat.us/pc/cambionCycle",
@@ -12,31 +17,30 @@ async def cycles():
     async with aiohttp.ClientSession() as session:
         tasks = []
         for link in links:
-            task = asyncio.create_task(session.get(link))
+            task = asyncio.create_task(get_req(link, session))
             tasks.append(task)
-        responses: list[aiohttp.ClientResponse] = await asyncio.gather(*tasks)
+        responses = await asyncio.gather(*tasks)
 
-    embed = nextcord.Embed(
-        title="Worldstates",
-        description="",
-        color=bot_basic_color,
-        timestamp=nextcord.utils.utcnow(),
-    )
-    for c, response in enumerate(responses):
-        data = await response.json()
-        if c == 0:
-            obtainable = "Fass" if data["active"] == "vome" else "Vome"
-            embed.description = f"**Cambion Drift**\nActive: {data['active'].capitalize()}\nTime left: {data['timeLeft']}\n\nObtainable residue: {obtainable}"
+        embed = nextcord.Embed(
+            title="Worldstates",
+            description="",
+            color=bot_basic_color,
+            timestamp=nextcord.utils.utcnow(),
+        )
+        for c, data in enumerate(responses):
+            if c == 0:
+                obtainable = "Fass" if data["active"] == "vome" else "Vome"
+                embed.description = f"**Cambion Drift**\nActive: {data['active'].capitalize()}\nTime left: {data['timeLeft']}\n\nObtainable residue: {obtainable}"
 
-        elif c == 1:
-            embed.description += (
-                f"\n\n\n**Plains of Eidolon (PoE)**\n{data['shortString']}"
-            )
+            elif c == 1:
+                embed.description += (
+                    f"\n\n\n**Plains of Eidolon (PoE)**\n{data['shortString']}"
+                )
 
-        elif c == 2:
-            embed.description += f"\n\n\n**Orb Vallis**\n{data['shortString']}"
+            elif c == 2:
+                embed.description += f"\n\n\n**Orb Vallis**\n{data['shortString']}"
 
-    embed.set_thumbnail(
-        "http://n9e5v4d8.ssl.hwcdn.net/uploads/dab77b032bfa299ad8e5807e8d989544.jpg"
-    )
-    return embed
+        embed.set_thumbnail(
+            "http://n9e5v4d8.ssl.hwcdn.net/uploads/dab77b032bfa299ad8e5807e8d989544.jpg"
+        )
+        return embed
