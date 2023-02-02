66a4a7029cd6e546d390ee9ce8207cc55e0f5bc4
Kai Konze <kaicrafter2000@gmail.com>
2023-02-02 18:52:47 +0100

    Fixed 'this interaction has already responded'

--- other/DeferTimer.py
+++ other/DeferTimer.py
@@ -4,18 +4,17 @@ import nextcord
 class DeferTimer:
     def __init__(self, interaction: nextcord.Interaction, timeout: float = 2.5) -> None:
         self.__timeout = timeout
-        self.cancel = False
         self.__interaction = interaction
 
-        self.do_debug = False
+        self.__do_debug = False
 
     async def start(self):
         await asyncio.sleep(self.__timeout)
 
-        if self.cancel:
+        if self.__interaction.response.is_done():
             return
 
-        if self.do_debug:
+        if self.__do_debug:
             print("Defered command!")
 
         await self.__interaction.response.defer()
